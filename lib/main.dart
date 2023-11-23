4cf6136e8c4e0eaacedff7730c846135d7134fac
Abolaji Ayeni <ayenibolaji07@gmail.com>
2023-11-23 17:57:49 +0100

    mvvm pattern implemented for clean architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:expatswap_fluttertask/splash_screen.dart';
+import 'package:expatswap_fluttertask/view/presentation/onboarding/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
