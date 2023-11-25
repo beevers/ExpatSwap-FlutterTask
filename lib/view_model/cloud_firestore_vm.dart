372dfe2c2322d7118532ce6a6f6e8d9aec9d3f4f
Abolaji Ayeni <ayenibolaji07@gmail.com>
2023-11-25 02:30:19 +0100

    added documentation on vm classes

--- lib/view_model/cloud_firestore_vm.dart
+++ lib/view_model/cloud_firestore_vm.dart
@@ -2,60 +2,82 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expatswap_fluttertask/model/user_model/user_model.dart';
 import 'package:flutter/material.dart';
 
+// ViewModel class responsible for interacting with Cloud Firestore
 class CloudFirestoreViewModel {
+  // Reference to the 'users' collection in Cloud Firestore
   final CollectionReference users =
       FirebaseFirestore.instance.collection('users');
 
-  Future<void> saveData(
-      {required String name,
-      required String email,
-      required String phone,
-      required String dob,
-      required String address}) {
+  // Method to save user data to Cloud Firestore
+  Future<void> saveData({
+    required String name,
+    required String email,
+    required String phone,
+    required String dob,
+    required String address,
+  }) {
+    // Add user data to the 'users' collection
     return users
         .add({
           'name': name,
           'email': email,
           'phone': phone,
           'dob': dob,
-          'address': address
+          'address': address,
         })
+        // Handle success by printing a debug message
         .then((value) => debugPrint("Data Added"))
+        // Handle errors by printing an error message
         .catchError((error) => debugPrint("Failed to add data: $error"));
   }
 
-  Stream<List<Map<String, dynamic>>> readUser() =>
-      users.snapshots().map((snapshot) => snapshot.docs.map((doc) {
+  // Method to read user data from Cloud Firestore as a stream of Maps
+  Stream<List<Map<String, dynamic>>> readUser() => users.snapshots().map(
+        (snapshot) => snapshot.docs.map(
+          (doc) {
+            // Extract user data from the document and map it to a Map
             final data = doc.data() as Map<String, dynamic>;
             return {
               'id': doc.id,
               'data': UserModel.fromJson(data),
             };
-          }).toList());
+          },
+        ).toList(),
+      );
 
+  // Method to delete user data from Cloud Firestore
   Future<bool> deleteData(String docId) async {
     try {
+      // Delete the document with the specified document ID
       await users.doc(docId).delete();
+      // Return true to indicate successful deletion
       return true;
     } catch (error) {
+      // Handle errors by printing an error message and return false
       debugPrint('Error deleting document: $error');
       return false;
     }
   }
 
-  Future<bool> updateData(
-      {required String docId, required UserModel user}) async {
+  // Method to update user data in Cloud Firestore
+  Future<bool> updateData({
+    required String docId,
+    required UserModel user,
+  }) async {
     try {
+      // Update the document with the specified document ID
       await users.doc(docId).update({
         'name': user.name,
         'phone': user.phone,
         'address': user.address,
         'email': user.email,
-        'dob': user.dob
+        'dob': user.dob,
       });
+      // Return true to indicate successful update
       return true;
     } catch (error) {
-      debugPrint('Error deleting document: $error');
+      // Handle errors by printing an error message and return false
+      debugPrint('Error updating document: $error');
       return false;
     }
   }
--- lib/view_model/date_picker_vm.dart
+++ lib/view_model/date_picker_vm.dart
@@ -2,11 +2,13 @@ import 'package:expatswap_fluttertask/data/controllers/form_controller/text_form
 import 'package:expatswap_fluttertask/model/abstracts/date_picker_abstract.dart';
 import 'package:flutter/material.dart';
 
+// ViewModel class responsible for handling date picking logic
 class DatePickerViewModel
     with ChangeNotifier
     implements DatePickerViewModelInterface {
   @override
   Future<void> pickDate(BuildContext context) async {
+    // Show the date picker dialog
     DateTime? date = (await showDatePicker(
       context: context,
       initialDate: DateTime.now().subtract(const Duration(days: 5136)),
@@ -14,9 +16,13 @@ class DatePickerViewModel
       lastDate: DateTime.now().subtract(const Duration(days: 3979)),
     ));
 
+    // Check if a date was selected
     if (date != null) {
+      // Update the text in the date of birth controller
       dobController.text = "${date.year}-${date.month}-${date.day}";
-      notifyListeners(); // Notify listeners if necessary
+
+      // Notify listeners that the state has changed
+      notifyListeners();
     }
   }
 }
